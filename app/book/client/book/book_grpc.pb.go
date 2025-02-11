5bb9be79ab41e3286075a74e859320f0c8066fe3
Faisal-althuwaini <faisal.yalthuwaini@gmail.com>
2025-02-11 10:08:53 +0300

    Creating Makefile

--- app/book/client/book/book_grpc.pb.go
+++ app/book/client/book/book_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.3
-// source: client/book/book.book
+// source: book.proto
 
 package book
 
@@ -72,7 +72,7 @@ func (c *bookServiceClient) DeleteBook(ctx context.Context, in *BookDeletionRequ
 	return out, nil
 }
 
-// BookServiceServer is the client API for BookService service.
+// BookServiceServer is the server API for BookService service.
 // All implementations must embed UnimplementedBookServiceServer
 // for forward compatibility.
 type BookServiceServer interface {
@@ -195,5 +195,5 @@ var BookService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "client/book/book.book",
+	Metadata: "book.proto",
 }
